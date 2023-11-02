587005a5668c7bd9a0c6f1381f8ea52ddd7a1980
José Cardoso <jose.cardoso2@paddypower.com>
2023-11-02 11:11:38 +0000

    feat(setup): graphql codegen basic setup

---
+++ apps/g-retro-api/codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: './src/graphql/schema',
+  documents: 'src/**/build-schema.ts',
+  generates: {
+    'src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
+
+export default config;
