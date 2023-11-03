db9da193e71b92767bfdf7a71cc20ff4ba0b790c
José Cardoso <jose.cardoso2@paddypower.com>
2023-11-03 10:59:48 +0000

    feat(setup): setup some plugins
    codegen-typescript , codegen-typescript-operations , codegen-typescript-resolvers and codegen-add

--- apps/g-retro-api/codegen.ts
+++ apps/g-retro-api/codegen.ts
@@ -1,19 +1,18 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "./src/graphql/schema",
-  documents: "./src/**/*.ts",
+  schema: './src/graphql/schema',
+  documents: './src/**/*.ts',
   generates: {
-    "./bin/graphql": {
-      preset: "client",
-      plugins: []
+    './bin/graphql': {
+      preset: 'client',
+      plugins: [],
     },
-    "./graphql.schema.json": {
-      plugins: ["introspection"]
-    }
-  }
+    './bin/graphql/schema/graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
 };
 
 export default config;
