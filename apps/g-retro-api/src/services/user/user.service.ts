d34673cc69a007097ce5d5470e63986f7efd761f
José Cardoso <jose.cardoso2@paddypower.com>
2024-01-30 12:24:54 +0000

    feat(user): add dataloader to user.service.getUserById

--- apps/g-retro-api/src/services/user/user.service.ts
+++ apps/g-retro-api/src/services/user/user.service.ts
@@ -1,14 +1,35 @@
+import DataLoader from 'dataloader';
+import { type EntityManager, In } from 'typeorm';
+
 import { UserEntity } from '../../entity';
 
-import type { EntityManager } from 'typeorm';
-
 export class UserService {
-  constructor(private entityManager: EntityManager) {}
+  private userByIdLoader: DataLoader<string, UserEntity | null, string>;
+
+  constructor(private entityManager: EntityManager) {
+    this.userByIdLoader = new DataLoader(async (idList: readonly string[]) => {
+      const users = await this.entityManager.find(UserEntity, {
+        where: {
+          id: In(idList),
+        },
+      });
+
+      const usersMap = users.reduce<Record<string, UserEntity>>((map, user) => {
+        map[user.id] = user;
+
+        return map;
+      }, {});
+
+      return idList.map(id => {
+        const user = usersMap[id];
+
+        return user ? user : null;
+      });
+    });
+  }
 
   public async getUserById(userId: string): Promise<UserEntity | Error> {
-    const user = await this.entityManager.findOneBy(UserEntity, {
-      id: userId,
-    });
+    const user = await this.userByIdLoader.load(userId);
 
     return user ? user : new Error(`User with id: ${userId} not found.`);
   }
