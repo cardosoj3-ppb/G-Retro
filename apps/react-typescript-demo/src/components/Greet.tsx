03437eaa302dc69969e2e8999de55a5fb8520c62
José Cardoso <jose.cardoso2@paddypower.com>
2024-02-21 12:22:55 +0000

    feat(components): basic props, using String, Number, Boolean, Objetcs & Arrays

--- apps/react-typescript-demo/src/components/Greet.tsx
+++ apps/react-typescript-demo/src/components/Greet.tsx
@@ -2,13 +2,19 @@ import { Hours } from "./Time";
 
 type GreetProps = {
     name: string
+    messageCount: number
+    isLoggedIn: Boolean
 }
 
 
 export const Greet = (props: GreetProps) => {
     return(
         <div>
-            <h2>Welcome back, {props.name}!</h2>
+            <h2>
+                {
+                    props.isLoggedIn ? `Welcome back, ${props.name}! You have ${props.messageCount} unread messages. ` : `Welcome Guest!`
+                }
+            </h2>
             <Hours/>
         </div>
     )
---
+++ apps/react-typescript-demo/src/components/Person.tsx
@@ -0,0 +1,10 @@
+type PersonProps = {
+    name: {
+        first: string
+        last: string
+    }
+}
+
+export const Person = (props: PersonProps) => {
+    return  <div>{props.name.first} {props.name.last}</div>
+}
