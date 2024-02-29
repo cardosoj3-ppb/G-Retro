f00a5330c76d9aaa1389995789d46fd29da375c1
José Cardoso <jose.cardoso2@paddypower.com>
2024-02-29 17:46:39 +0000

    feat(usecontext): useContext hook

---
+++ apps/react-typescript-demo/src/components/context/Box.tsx
@@ -0,0 +1,7 @@
+import { useContext } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+export const Box = () => {
+  const theme = useContext(ThemeContext);
+  return <div style={{backgroundColor: theme.primary.main , color: theme.primary.text}}> Theme Context</div>;
+};
